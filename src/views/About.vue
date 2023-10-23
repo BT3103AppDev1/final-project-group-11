9e1b991bf64f79cfa451ea51caa3d16b84d92a71
Justin Lum <justinlum7@gmail.com>
2023-10-23 14:22:42 +0800

    Simple template for about.vue

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <div style = "text-align:center;" v-if = "user">
+        <NavBar/>
+        <h2> This is the about page for CompareCart! </h2>
+        <h3> This is the about page for CompareCart. A web application that is able to compare the different prices from different ecommerce websites to help you with your decisions
+        on which product would be most enticing for you! </h3>
+    </div>
+</template>
