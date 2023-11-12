aecfe288bba495f05db254bfe028ad5155bd2bb9
Aarushi <mundraarushi@gmail.com>
2023-11-12 11:36:41 +0800

    edited PriceTracker.vue

--- src/components/PriceTracker.vue
+++ src/components/PriceTracker.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="price-tracker">
     <h3>Price Tracker</h3>
-    <div v-if="user">
-      <canvas ref="lineChart" width="400" height="200"></canvas>
-    </div>
-    <div v-else>
-      <p class="error-message">Sign in to view price history.</p>
-    </div>
+    <canvas ref="lineChart" width="400" height="200"></canvas>
   </div>
 </template>
 
@@ -14,37 +9,17 @@
 import { ref, onMounted } from "vue";
 import { Chart, registerables } from "chart.js/auto";
 Chart.register(...registerables);
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
 export default {
-  data() {
-    return {
-      user: false,
-      useremail: "",
-    };
-  },
-  mounted() {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        this.user = user;
-        this.useremail = user.email;
-      }
-    });
-  },
   props: {
     PreviousPrices: Array,
   },
   setup(props) {
     const lineChart = ref(null);
-
     onMounted(() => {
       // Use Chart.js to create the line chart
       const ctx = lineChart.value.getContext("2d");
       updateChart(ctx, props.PreviousPrices);
     });
-
     function updateChart(ctx, prices) {
       console.log("Received prices:", prices);
       new Chart(ctx, {
@@ -68,7 +43,6 @@ export default {
         },
       });
     }
-
     return {
       lineChart,
     };
@@ -81,11 +55,5 @@ export default {
   max-width: 400px;
   margin: 0 auto;
   text-align: center;
-}
-.error-message {
-  color: red;
-  border: 1px solid red;
-  padding: 10px;
-  border-radius: 5px;
 }
 </style>
